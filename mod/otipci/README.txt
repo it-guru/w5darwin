455f6cc5dc7e8d1aa4b13f82bb69dcc2a7821703
Hartmut Vogler <hartmut.vogler@t-systems.com>
2026-01-23 12:36:47 +0100

    add 1st Version of otip_ci interface based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/17507627280001
    (cherry picked from commit 7589402047c3b7af6f8deb4101b6423f65d8e834)

---
+++ mod/otipci/README.txt
@@ -0,0 +1,6 @@
+Doku:
+https://yam-united.telekom.com/pages/otip/apps/content/content
+
+API in Tardis:
+https://developer.telekom.de/catalog/otip/otip-telit-ci/t_systems/preproduction/1.0.9
+https://developer.telekom.de/catalog/otip/otip-telit-ci/t_systems/production/1.0.8
---
+++ mod/otipci/system.pm
@@ -0,0 +1,200 @@
+package otipci::system;
+#  W5Base Framework
+#  Copyright (C) 2026  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+
+use kernel;
+use kernel::Field;
+use kernel::App::Web::Listedit;
+use kernel::DataObj::ElasticSearch;
+use otipci::lib::Listedit;
+use JSON;
+use MIME::Base64;
+@ISA=qw(kernel::App::Web::Listedit kernel::DataObj::ElasticSearch 
+        otipci::lib::Listedit);
+
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   $self->AddFields(
+      new kernel::Field::Id(     
+            name          =>'id',
+            searchable    =>0,
+            group         =>'source',
+            dataobjattr   =>'_id',
+            label         =>'Id'),
+
+      new kernel::Field::RecordUrl(),
+
+      new kernel::Field::Text(     
+            name          =>'fullname',
+            searchable    =>0,
+            dataobjattr   =>'_source.fullname',
+            label         =>'fullname'),
+
+      new kernel::Field::Text(     
+            name          =>'name',
+            searchable    =>0,
+            dataobjattr   =>'_source.u_real_name',
+            label         =>'name'),
+
+      new kernel::Field::Text(     
+            name          =>'systemid',
+            dataobjattr   =>'_source.u_external_id',
+            label         =>'SystemID'),
+
+      new kernel::Field::Text(     
+            name          =>'sys_id',
+            searchable    =>0,
+            group         =>'source',
+            dataobjattr   =>'_source.sys_id',
+            label         =>'ServiceNow sys_id'),
+
+      new kernel::Field::Text(     
+            name          =>'class',
+            searchable    =>0,
+            group         =>'source',
+            dataobjattr   =>'_source.class',
+            label         =>'class'),
+
+      new kernel::Field::Text(     
+            name          =>'category',
+            searchable    =>0,
+            group         =>'source',
+            dataobjattr   =>'_source.category',
+            label         =>'category'),
+
+      new kernel::Field::CDate(
+            name          =>'cdate',
+            group         =>'source',
+            label         =>'Creation-Date',
+            dataobjattr   =>'_source.sys_created_on'),
+
+      new kernel::Field::MDate(
+            name          =>'mdate',
+            group         =>'source',
+            label         =>'Modification-Date',
+            dataobjattr   =>'_source.sys_updated_on'),
+
+
+
+      new kernel::Field::Date(     
+            name          =>'srcload',
+            dataobjattr   =>'_source.otip_version',
+            ElasticType   =>'keyword',
+            searchable    =>0,
+            group         =>'source',
+            htmldetail    =>'NotEmpty',
+            label         =>'Source-Load')
+
+   );
+
+# "otip_version": "2025-12-01T06:05:44.0Z",
+#    "sys_id": "7a95c2f36bbcf0d047df9974ab63fb81",
+#    "otip_id": "7a95c2f36bbcf0d047df9974ab63fb81",
+#    "otip_deleted": true,
+#    "class": "cmdb_ci_server",
+#    "u_mandator_key": "A000A53E.000000"
+
+
+   $self->setDefaultView(qw(id ));
+   $self->LimitBackend(10000);
+   return($self);
+}
+
+
+sub getCredentialName
+{
+   my $self=shift;
+
+   return("otipci");
+}
+
+
+sub ESindexName
+{
+   my $self=shift;
+
+   return("otipci__all");
+}
+
+
+sub SetFilter
+{
+   my $self=shift;
+
+   my $flt=$_[0];
+
+   if (ref($flt) eq "HASH"){
+      $_[0]->{class}="cmdb_ci_server";
+   }
+
+   return($self->SUPER::SetFilter(@_));
+}
+
+
+
+
+
+sub getDetailBlockPriority
+{
+   my $self=shift;
+   my $grp=shift;
+   my %param=@_;
+   return(qw(header default source));
+}
+
+
+
+sub isViewValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return("default") if (!defined($rec));
+   return("ALL");
+}
+
+sub isWriteValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return(undef);
+}
+
+sub isQualityCheckValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return(0);
+}
+
+sub isUploadValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return(0);
+}
+
+
+
+1;
